bb1c30ae9dd31c1036914b0439675bdc49579dd6
Wesley <wesley16838@gmail.com>
2019-05-14 17:31:30 -0400

    update code

--- frontend/data/index.js
+++
@@ -1,8 +0,0 @@
-const userData = require("./user");
-const bookData = require("./book");
-const cartData = require("./cart");
-module.exports = {
-  user: userData,
-  book: bookData,
-  cart: cartData
-};
